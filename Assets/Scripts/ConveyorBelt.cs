c18dca8fac9781fcf3e06374e832153788e91ac4
Trevor Cardoza <trevor.cardoza1538@gmail.com>
2021-12-03 20:13:44 -0800

    We now have four motion scenes with a total of 5 motions (the teleportation one my not work :cold_sweat: )

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class ConveyorBelt : MonoBehaviour
 {
     public GameObject belt;
+    public Transform pointToGoTo;
     public Transform endpoint;
+    public Transform endpoint2;
     public float speed;
+
+    private void Start()
+    {
+        pointToGoTo.position = endpoint.position;
+        
+    }
 
     private void OnTriggerStay(Collider other)
     {
-        other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.position, speed * Time.deltaTime);
+        if (Vector3.Distance(other.gameObject.transform.position, pointToGoTo.position) <= 1)
+        {
+            if (Vector3.Distance(other.gameObject.transform.position, endpoint.position) <= 1)
+            {
+                pointToGoTo.position = endpoint2.position;
+            }
+            else if (Vector3.Distance(other.gameObject.transform.position, endpoint2.position) <= 1)
+            {
+                pointToGoTo.position = endpoint.position;
+            }
+        }
+        other.transform.position = Vector3.MoveTowards(other.transform.position, pointToGoTo.position, speed * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/Joystick_Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joystick_Controller : MonoBehaviour
+{ 
+	GameObject Hand;
+	GameObject Player;
+	float rotationValue;
+	float rotationInput;
+	float movementValue;
+	float movementInputx;
+	float movementInputy;
+	// Start is called before the first frame update
+	void Start()
+	{
+		Player = this.gameObject;
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		rotationInput = Input.GetAxis("Xbox Right Joystick Horizontal") + OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x;
+		movementInputx = Input.GetAxis("Horizontal") + OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x;
+		movementInputy = Input.GetAxis("Vertical") + OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).y;
+		if (movementInputx != 0 || movementInputy != 0)
+		{
+			print("X Movement: "+Mathf.Clamp(-1 * movementInputx, -0.07f, 0.07f));
+			print("Y Movement: "+Mathf.Clamp(-1 * movementInputy, -0.07f, 0.07f));
+			var playerMovement = new Vector3(Mathf.Clamp(movementInputx, -0.07f, 0.07f), 0, Mathf.Clamp(movementInputy, -0.07f, 0.07f));
+			Player.transform.Translate(playerMovement,Space.Self);
+		}
+		if (rotationInput != 0)
+		{
+			rotationValue += rotationInput * 0.5f;
+			print(rotationValue);
+			Player.transform.rotation = Quaternion.Euler(new Vector3(0, rotationValue, 0));
+		}
+	}
+}
